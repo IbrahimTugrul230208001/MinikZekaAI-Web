e116c614f5f806c03f996c7f66edb98edba6973a
IbrahimTugrul230208001 <230208001@ostimteknik.edu.tr>
2025-07-29 10:23:31 +0300

    added other fundamentals (SignalR, DB)

--- MinikZekaAI-Web.DataAccess/MinikZekaContext.cs
+++ MinikZekaAI-Web.DataAccess/MinikZekaContext.cs
@@ -4,6 +4,13 @@ namespace MinikZekaAI_Web.DataAccess
 {
     public class MinikZekaContext :DbContext
     {
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Password=ibrahim06;Database=MinikZekaDB;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
